bb9e08107420f77a2d3dcf3f1fe8037be34350de
Kyra Zhang <qilanz@andrew.cmu.edu>
2022-09-26 21:18:38 -0700

    add SN tests

--- test/TestSNWithMockDAO.java
+++ test/TestSNWithMockDAO.java
@@ -27,22 +27,22 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
 		daoFactory = DAOFactory.getInstance();
 		accountDAO = daoFactory.getAccountDAOMock();
 		sn = new SocialNetwork(accountDAO);
-		m1 = sn.join("m1");
-		m2 = sn.join("m2");
-		m3 = sn.join("m3");
-		m4 = sn.join("m4");
-		m5 = sn.join("m5");
+		m1 = sn.join("John");
+		m2 = sn.join("Hakan");
+		m3 = sn.join("Serra");
+		m4 = sn.join("Dean");
+		m5 = sn.join("Hasan");
 		all.add(m1);
 		all.add(m2);
 		all.add(m3);
 		all.add(m4);
 		all.add(m5);
 		if (DAOFactory.isMock(accountDAO)) {
-			when(accountDAO.findByUserName("m1")).thenReturn(m1);
-			when(accountDAO.findByUserName("m2")).thenReturn(m2);
-			when(accountDAO.findByUserName("m3")).thenReturn(m3);
-			when(accountDAO.findByUserName("m4")).thenReturn(m4);
-			when(accountDAO.findByUserName("m5")).thenReturn(m5);
+			when(accountDAO.findByUserName("John")).thenReturn(m1);
+			when(accountDAO.findByUserName("Hakan")).thenReturn(m2);
+			when(accountDAO.findByUserName("Serra")).thenReturn(m3);
+			when(accountDAO.findByUserName("Dean")).thenReturn(m4);
+			when(accountDAO.findByUserName("Hasan")).thenReturn(m5);
 			when(accountDAO.findAll()).thenReturn(all);
 		}
 	}
@@ -50,6 +50,7 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
 	/* 
 	 * Generic tests are automatically inherited from abstract superclass - they should continue to work here! 
 	 */
+
 	@Test
 	public void canLoginAndFindYourself() throws NoUserLoggedInException,
 			UserNotFoundException {
@@ -65,6 +66,14 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
 		assertTrue(sn.hasMember(m3.getUserName()));
 	}
 
+	@Test(expected = NoUserLoggedInException.class)
+	public void logout() throws UserNotFoundException,
+			NoUserLoggedInException {
+		sn.login(m3);
+		sn.logout();
+		sn.sendFriendshipTo("Hakan");
+	}
+
 	@Test
 	public void canListMembers() throws NoUserLoggedInException,
 			UserNotFoundException {
@@ -74,6 +83,35 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
 		assertTrue(members.contains(m3.getUserName()));
 	}
 
+
+	@Test
+	public void acceptingAllFriendRequests() throws UserNotFoundException,
+			NoUserLoggedInException {
+		sn.login(m1);//John
+		sn.sendFriendshipTo("Serra");
+		sn.login(m2);//Hakan
+		sn.sendFriendshipTo("Serra");
+		sn.login(m3);//Serra
+		sn.acceptAllFriendships();
+		assertEquals(0, m3.getIncomingRequests().size());
+		assertEquals(2, m3.getFriends().size());
+		assertTrue(m3.getFriends().contains("John"));
+		assertTrue(m3.getFriends().contains("Hakan"));
+	}
+
+	@Test
+	public void sendFriendshipCancellationTo() throws UserNotFoundException,
+			NoUserLoggedInException {
+		sn.login(m1);//John
+		sn.sendFriendshipTo("Serra");//m3
+		sn.login(m3);//Serra
+		sn.acceptAllFriendships();
+		sn.login(m1);//John
+		sn.sendFriendshipCancellationTo("Serra");
+		assertEquals(0, m3.getIncomingRequests().size());
+		assertEquals(0, m1.getOutgoingRequests().size());
+	}
+	
 	@Test
 	public void sendingFriendRequestCreatesPendingRequestAndResponse()
 			throws UserNotFoundException, NoUserLoggedInException {
@@ -118,7 +156,7 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
 
 	@Test(expected = UserExistsException.class)
 	public void canNotJoinSocialNetworkAgain() throws UserExistsException {
-		sn.join("m1");
+		sn.join("John");
 	}
 
 	@Test
@@ -172,13 +210,62 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
 		sn.leave();
 		if (DAOFactory.isMock(accountDAO)) {
 			all.remove(m2);
-			when(accountDAO.findByUserName("m2")).thenReturn(null);
+			when(accountDAO.findByUserName("Hakan")).thenReturn(null);
 		}
 		// might have to do additional checking if using a Mockito mock
 		sn.login(m3);
 		assertFalse(sn.hasMember(m2.getUserName()));
 	}	
 	
+	@Test
+	public void recommendFriendsAlreadyFriends() throws UserNotFoundException,
+			NoUserLoggedInException {
+		sn.login(m1);
+		sn.sendFriendshipTo(m2.getUserName());
+		sn.sendFriendshipTo(m3.getUserName());
+		sn.sendFriendshipTo(m4.getUserName());
+		sn.login(m2);
+		sn.acceptFriendshipFrom(m1.getUserName());
+		sn.sendFriendshipTo(m4.getUserName());
+		sn.login(m3);
+		sn.acceptFriendshipFrom(m1.getUserName());
+		sn.sendFriendshipTo(m4.getUserName());
+		sn.login(m4);
+		sn.acceptFriendshipFrom(m1.getUserName());
+		sn.acceptFriendshipFrom(m2.getUserName());
+		sn.acceptFriendshipFrom(m3.getUserName());
+		sn.login(m1);
+		Set<String> recommendations = sn.recommendFriends();
+		assertFalse(recommendations.contains(m4.getUserName()));
+		assertFalse(recommendations.contains(m3.getUserName()));
+		assertFalse(recommendations.contains(m1.getUserName()));
+		assertEquals(0, recommendations.size());
+	}
+
+	@Test
+	public void recommendFriendsBlockedAccount() throws UserNotFoundException,
+			NoUserLoggedInException {
+		sn.login(m1);
+		sn.sendFriendshipTo(m2.getUserName());
+		sn.sendFriendshipTo(m3.getUserName());
+		sn.block(m4.getUserName());
+		sn.login(m2);
+		sn.acceptFriendshipFrom(m1.getUserName());
+		sn.sendFriendshipTo(m4.getUserName());
+		sn.login(m3);
+		sn.acceptFriendshipFrom(m1.getUserName());
+		sn.sendFriendshipTo(m4.getUserName());
+		sn.login(m4);
+		sn.acceptFriendshipFrom(m2.getUserName());
+		sn.acceptFriendshipFrom(m3.getUserName());
+		sn.login(m1);
+		Set<String> recommendations = sn.recommendFriends();
+		assertFalse(recommendations.contains(m4.getUserName()));
+		assertFalse(recommendations.contains(m3.getUserName()));
+		assertFalse(recommendations.contains(m1.getUserName()));
+		assertEquals(0, recommendations.size());
+	}
+
 	/* 
 	 * VERIFICATION TESTS
 	 * 
@@ -198,7 +285,7 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
         throws UserNotFoundException, UserExistsException, NoUserLoggedInException {
 		// make sure that when a logged-in member issues a friend request, any changes to the affected accounts will be persisted
 		sn.login(m1);
-		sn.sendFriendshipTo("m2");
+		sn.sendFriendshipTo("Hakan");
 		verify(accountDAO, times(1)).update(eq(m1));
 		verify(accountDAO, times(1)).update(eq(m2));
 	}
@@ -207,9 +294,9 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
         throws UserNotFoundException, UserExistsException, NoUserLoggedInException {
 		// make sure that when a logged-in member issues a friend request, any changes to the affected accounts will be persisted
 		sn.login(m1);
-		sn.sendFriendshipTo("m2");
+		sn.sendFriendshipTo("Hakan");
 		sn.login(m2);
-		sn.acceptFriendshipFrom("m1");
+		sn.acceptFriendshipFrom("John");
 		verify(accountDAO, times(2)).update(eq(m1));
 		verify(accountDAO, times(2)).update(eq(m2));
 	}
@@ -218,9 +305,9 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
         throws UserNotFoundException, UserExistsException, NoUserLoggedInException {
 		// make sure that when a logged-in member rejects a friend request, any changes to the affected accounts will be persisted
 		sn.login(m1);
-		sn.sendFriendshipTo("m2");
+		sn.sendFriendshipTo("Hakan");
 		sn.login(m2);
-		sn.rejectFriendshipFrom("m1");
+		sn.rejectFriendshipFrom("John");
 		verify(accountDAO, times(2)).update(eq(m1));
 		verify(accountDAO, times(2)).update(eq(m2));
 	}
@@ -229,7 +316,7 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
         throws UserNotFoundException, UserExistsException, NoUserLoggedInException {
 		// make sure that when a logged-in member blocks another member, any changes to the affected accounts will be persisted
 		sn.login(m1);
-		sn.block("m2");
+		sn.block("Hakan");
 		verify(accountDAO, times(1)).update(eq(m1));
 		verify(accountDAO, times(1)).update(eq(m2));
 	}
@@ -239,10 +326,10 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
 		// make sure that when a logged-in member leaves the social network, his account will be permanenlty deleted and  
 		// any changes to the affected accounts will be persisted
 		sn.login(m1);
-		sn.sendFriendshipTo("m2");
-		sn.sendFriendshipTo("m3");
+		sn.sendFriendshipTo("Hakan");
+		sn.sendFriendshipTo("Serra");
 		sn.login(m2);
-		sn.acceptFriendshipFrom("m1");
+		sn.acceptFriendshipFrom("John");
 		sn.login(m1);
 		sn.leave();
 		verify(accountDAO, times(1)).delete(eq(m1));
@@ -250,4 +337,55 @@ public class TestSNWithMockDAO extends TestSNAbstractGeneric {
 		verify(accountDAO, times(2)).update(eq(m3)); //m1 request, m1 leave
 	}
 
+	@Test
+	public void rejectingAllFriendRequests() throws UserNotFoundException,
+			NoUserLoggedInException {
+		sn.login(m1);//John
+		sn.sendFriendshipTo("Serra");
+		sn.login(m2);//Hakan
+		sn.sendFriendshipTo("Serra");
+		sn.login(m3);//Serra
+		sn.rejectAllFriendships();
+		assertEquals(0, m3.getIncomingRequests().size());
+		//TODO
+	}
+	
+	@Test
+	public void unblock() throws UserNotFoundException,
+			NoUserLoggedInException {
+		sn.login(m1);
+		sn.block("Hakan");//m2
+		sn.unblock("Hakan");
+		assertTrue(sn.listMembers().contains("Hakan"));
+		sn.sendFriendshipTo("Hakan");
+		assertEquals(1, m1.getOutgoingRequests().size());
+		assertEquals(1, m2.getIncomingRequests().size());
+	}
+
+	@Test
+	public void autoAcceptFriendships() throws UserNotFoundException,
+			NoUserLoggedInException {
+		sn.login(m3);
+		sn.autoAcceptFriendships();
+		sn.login(m1);//John
+		sn.sendFriendshipTo("Serra");//m3
+		sn.login(m2);//Hakan
+		sn.sendFriendshipTo("Serra");//m3
+		assertEquals(2, m3.getFriends().size());
+	}
+
+	@Test
+	public void cancelAutoAcceptFriendships() throws UserNotFoundException,
+			NoUserLoggedInException {
+		sn.login(m3);
+		sn.autoAcceptFriendships();
+		sn.login(m1);//John
+		sn.sendFriendshipTo("Serra");//m3
+		sn.login(m3);
+		sn.cancelAutoAcceptFriendships();
+		sn.login(m2);//Hakan
+		sn.sendFriendshipTo("Serra");//m3
+		assertEquals(1, m3.getFriends().size());
+		assertTrue(m3.getFriends().contains("John"));
+	}
 }
